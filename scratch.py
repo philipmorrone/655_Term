e2f5fa99d032b1dfb3700aa928393efd666c3ec8
Philip Morrone <philip.j.morrone@gmail.com>
2021-11-22 22:00:22 -0600

    .

--- scratch.py
+++ scratch.py
@@ -23,12 +23,56 @@ G.add_edges_from(edges)
 
 print(nx.info(G))
 
-degree_dict = dict(G.degree(G.nodes()))
-sorted_degree = sorted(degree_dict.items(), key=itemgetter(1), reverse=True)
+hist_sig_dict = {}
+gender_dict = {}
+birth_dict = {}
+death_dict = {}
+id_dict = {}
 
-print("Top 20 nodes by degree:")
-for d in sorted_degree[:20]:
-    print(d)
+# Create an empty dictionary for each attribute
+hist_sig_dict = {}
+gender_dict = {}
+birth_dict = {}
+death_dict = {}
+id_dict = {}
+
+for node in nodes: # Loop through the list of nodes, one row at a time
+    hist_sig_dict[node[0]] = node[1] # Access the correct item, add it to the corresponding dictionary
+    gender_dict[node[0]] = node[2]
+    birth_dict[node[0]] = node[3]
+    death_dict[node[0]] = node[4]
+    id_dict[node[0]] = node[5]
+
+# Add each dictionary as a node attribute to the Graph object
+nx.set_node_attributes(G, hist_sig_dict, 'historical_significance')
+nx.set_node_attributes(G, gender_dict, 'gender')
+nx.set_node_attributes(G, birth_dict, 'birth_year')
+nx.set_node_attributes(G, death_dict, 'death_year')
+nx.set_node_attributes(G, id_dict, 'sdfb_id')
+
+# Loop through each node, to access and print all the "birth_year" attributes
+#for n in G.nodes():
+    #print(n, G.nodes[n]['historical_significance'])
+
+betweenness_dict = nx.betweenness_centrality(G) # Run betweenness centrality
+eigenvector_dict = nx.eigenvector_centrality(G) # Run eigenvector centrality
+degree_dict = dict(G.degree(G.nodes()))
+nx.set_node_attributes(G, degree_dict, 'degree')
+
+
+# Assign each to an attribute in your network
+nx.set_node_attributes(G, betweenness_dict, 'betweenness')
+nx.set_node_attributes(G, eigenvector_dict, 'eigenvector')
+
+#First get the top 20 nodes by betweenness as a list
+sorted_betweenness = sorted(betweenness_dict.items(), key=itemgetter(1), reverse=True)
+top_betweenness = sorted_betweenness[:20]
+
+#Then find and print their degree
+for tb in top_betweenness: # Loop through top_betweenness
+    degree = degree_dict[tb[0]] # Use degree_dict to access a node's degree, see footnote 2
+    print("Name:", tb[0], "| Betweenness Centrality:", tb[1], "| Degree:", degree)
+
 
 nx.write_gexf(G, "test11.gexf")
 
